7e487f52e056387e1ded812e2d032aeea1d49e21
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2025-04-08 14:00:58 +0200

    rotation method

--- src/core/ocr/utils.py
+++ src/core/ocr/utils.py
@@ -1,11 +1,13 @@
-import cv2
 import numpy as np
 import pytesseract
-from scipy import ndimage
+from PIL import Image
 
+from src.conf_logger import setup_logger
 from src.config import MINIO_READER_ACCESS_KEY, MINIO_READER_SECRET_KEY
 from src.core.filestorage.utils import S3ImageReader
 from src.core.ocr.exceptions import FileNotFoundInBucket
+
+logger = setup_logger(__name__, "ocr")
 
 
 class PytesseractReader:
@@ -13,53 +15,28 @@ class PytesseractReader:
         self.reader = S3ImageReader(MINIO_READER_ACCESS_KEY, MINIO_READER_SECRET_KEY)
 
     @staticmethod
-    def rotate_image_cv2(image: np.ndarray, angle: float) -> np.ndarray:
-        (h, w) = image.shape[:2]
-        center = (w/2, h/2)
+    def rotate_image_pil(image: Image.Image, angle: int) -> Image.Image:
+        return image.rotate(-angle, expand=True)
 
-        rotation_matrix = cv2.getRotationMatrix2D(center, -angle, 1.0)
-        rotated_image = cv2.warpAffine(image, rotation_matrix, (w, h),
-                                       flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REPLICATE)
-
-        return rotated_image
-
-    @staticmethod
-    def rotate_image_scipy(image: np.ndarray, angle: float) -> np.ndarray:
-        rotated_image = ndimage.rotate(image, angle=angle, reshape=True)
-
-        return rotated_image
-
-
-    def ocr_rotated_by_cv2(self, image) -> dict[str, list]:
-        osd_data = pytesseract.image_to_osd(image, output_type=pytesseract.Output.DICT)
-        rotation_angle = osd_data.get("rotate", 0)
-
-        image_rotated = image
-        if rotation_angle:
-            image_rotated = self.rotate_image_cv2(image, rotation_angle) if rotation_angle else image
-
-        result = pytesseract.image_to_data(image_rotated, config=r'--psm 4', output_type=pytesseract.Output.DICT)
-        return result
-
-    def ocr_rotated_by_scipy(self, image) -> dict[str, list]:
-        osd_data = pytesseract.image_to_osd(image, output_type=pytesseract.Output.DICT)
-        rotation_angle = osd_data.get("rotate", 0)
-
-        image_rotated = image
-        if rotation_angle:
-            image_rotated = self.rotate_image_scipy(image, rotation_angle) if rotation_angle else image
-
-        result = pytesseract.image_to_data(image_rotated, config=r'--psm 4', output_type=pytesseract.Output.DICT)
-        return result
+    def ocr_rotated_by_pillow(self, image: Image.Image, file_name: str = "unknown") -> Image.Image:
+        try:
+            osd_data = pytesseract.image_to_osd(image, output_type=pytesseract.Output.DICT)
+            rotation_angle = osd_data.get("rotate", 0)
+        except Exception as e:
+            logger.error(e)
+            rotation_angle = 0
+        response = self.rotate_image_pil(image, rotation_angle) if rotation_angle else image
+        logger.debug(f"Image: {file_name}, of sizes: {image.size} "
+                     f"{f'was rotated by {rotation_angle}' if rotation_angle else 'Was not rotated'}")
+        return response
 
     def ocr_file(self, file_name: str) -> dict[str, list]:
         with self.reader as bucket_connector:
-            file_blop = bucket_connector.get_image_as_numpy(file_name=file_name)
+            file_blop = bucket_connector.get_image_as_pil(file_name=file_name)
             if not np.any(file_blop):
                 raise FileNotFoundInBucket(message=f'File: {file_name}')
 
-        ocr_text = dict()
-        ocr_text = self.ocr_rotated_by_cv2(file_blop)
+        rotated_image = self.ocr_rotated_by_pillow(file_blop, file_name=file_name)
+        ocr_text = pytesseract.image_to_data(rotated_image, config=r'--psm 4', output_type=pytesseract.Output.DICT)
 
         return ocr_text
-
